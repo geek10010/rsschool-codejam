0f1e3e5feb4c94c65fced7ffd98ee256a80a6094
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-10-28 14:32:00 +0300

    test: add tests for make function

--- test.js
+++ test.js
@@ -27,3 +27,31 @@ describe('sumOfOther', () => {
     assert.deepStrictEqual(result, []);
   });
 });
+
+
+const make = require('./src/make');
+
+describe('make', () => {
+  const sum = (a, b) => a + b;
+  const mult = (a, b) => a * b;
+
+  it('1 sum', () => {
+    const result = make(15)(34, 21, 666)(41)(sum);
+    assert.equal(result, 777);
+  });
+
+  it('2 sum, additional arguments', () => {
+    const result = make(15, 4)(34, 21, 666)(41, 7)(sum);
+    assert.equal(result, 788);
+  });
+
+  it('3 multiply', () => {
+    const result = make(2)(3, 4)(5)(mult);
+    assert.equal(result, 120);
+  });
+
+  it('4 multiply, additional', () => {
+    const result = make(2, 4)(3, 4)(5, 3)(mult);
+    assert.equal(result, 1440);
+  });
+});
