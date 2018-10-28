7371d3b508603a097e18324c83880e1a824f135a
Dzmitry Tarasau <36638017+geek10010@users.noreply.github.com>
2018-10-28 14:44:20 +0300

    test: add test for recursion function

--- test.js
+++ test.js
@@ -55,3 +55,19 @@ describe('make', () => {
     assert.equal(result, 1440);
   });
 });
+
+
+const recursion = require('./src/recursion');
+
+describe('recursion', () => {
+  it('1', () => {
+    const tree = {
+      value: 100,
+      left: { value: 90, left: { value: 70 }, right: { value: 99 } },
+      right: { value: 120, left: { value: 110 }, right: { value: 130 } },
+    };
+    const array = recursion(tree);
+
+    assert.deepStrictEqual(array, [[100], [90, 120], [70, 99, 110, 130]]);
+  });
+});
